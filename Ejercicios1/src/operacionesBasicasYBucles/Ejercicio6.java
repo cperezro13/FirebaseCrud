d034349bc90bf10a8c5a1450ba66a49582d2e05f
Cristian <cristianalejandro172431@gmail.com>
2024-11-19 22:40:21 -0500

    Añadidos primeros ejercicios del 1 al 6

---
+++ Ejercicios1/src/operacionesBasicasYBucles/Ejercicio6.java
@@ -0,0 +1,12 @@
+
+package operacionesBasicasYBucles;
+
+public class Ejercicio6 {
+
+    public static void main(String[] args) {
+        int n1 = 1234;
+        int n2 = 532;
+        int sum = n1 * n2;
+        System.out.println(sum);
+    }
+}
